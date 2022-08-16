b2e265282acce30ef5785bb363e079b7e177220b
George Nabil <georgenabilpro@gmail.com>
2022-08-16 15:46:56 +0200

    added templates and added a new app

--- chess_pairing/helloworld/urls.py
+++ chess_pairing/helloworld/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("george", views.greet1, name="greet_george"),
+    path("<str:name>", views.greet2, name="greeting_someone"),
 ]
--- chess_pairing/helloworld/views.py
+++ chess_pairing/helloworld/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def idex(request):
-    return HttpResponse("Hello, world")
+def index(request):
+    return render(
+        request,
+        template_name="helloworld/index.html"
+    )
 
+def greet(request, name:str) -> HttpResponse:
+    return HttpResponse(f"hello, {name.capitalize()}")
 
+def greet1(reqest):
+    return HttpResponse("hello Georege!")
 
+def greet2(request, name:str):
+    return render(
+        request,
+        template_name="helloworld/greet.html",
+        context={
+            "name": name.capitalize()
+        }
+    )
